187a51ab4dcffef7e84e32766cb459bf9e4aa620
Mateusz Milczarek <mateusz.milczarek1@uth.pl>
2025-04-05 23:08:55 +0200

    Dodawanie do wyników do pliku - narazie wyniki zapisują sie w jednej linii

--- src/main/java/Kalkulator.java
+++ src/main/java/Kalkulator.java
@@ -1,13 +1,15 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Kalkulator  {
-//    public static void wykonajDzialanie(String dzialanie, int wynik, ArrayList wyniki){
-//        String wykonaneDzialanie = dzialanie + wynik;
-//        wyniki.add(wykonaneDzialanie);
-//        System.out.println(wykonaneDzialanie);
-//    }
+    public static void wykonajDzialanie(String dzialanie, int wynik, ArrayList wyniki){
+        String wykonaneDzialanie = dzialanie + wynik;
+        wyniki.add(wykonaneDzialanie);
+        System.out.println(wykonaneDzialanie);
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> wyniki = new ArrayList<>();
@@ -62,21 +64,35 @@ public class Kalkulator  {
             int wybor;
             boolean czyWybranaCyfra = false;
             while(!czyWybranaCyfra){
-                System.out.println("Wybierz '0' jeżeli chcesz wykonać kolejne działanie...");
-                System.out.println("Wybierz '1' jeżeli chcesz sprawdzić ostatnie działania'");
-                System.out.println("Wybierz '2' jeżeli chcesz wyjść z programu");
+                System.out.println("Wybierz '1' jeżeli chcesz wykonać kolejne działanie...");
+                System.out.println("Wybierz '2' jeżeli chcesz sprawdzić ostatnie działania'");
+                System.out.println("Wybierz '0' jeżeli chcesz wyjść z programu");
                 wybor = Integer.parseInt(scanner.nextLine());
                 switch (wybor) {
-                    case 0:
+                    case 1:
                         czyWybranaCyfra = true;
                         break;
-                    case 1:
+                    case 2:
                         System.out.println("Wykonano następujące działania: ");
                         for (String wynik : wyniki) {
                             System.out.println(wynik);
                         }
+                        System.out.println("Wciśnij '9' jeśli chcesz zapisać plik lub inny klawisz aby wrócić do menu ");
+                        int zapisDoPliku = Integer.parseInt(scanner.nextLine());
+                        if(zapisDoPliku == 9){
+                            FileWriter fw = null;
+                            try {
+                                fw = new FileWriter("plik.txt");
+                                for (String wynik : wyniki) {
+                                    fw.append(wynik);
+                                }
+                                fw.close();
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            };
+                        }
                         break;
-                    case 2:
+                    case 0:
                         czyWyjscie = true;
                         break;
                     default:
