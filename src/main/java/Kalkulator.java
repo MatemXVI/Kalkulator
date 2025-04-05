ac3756a86b6e7887ad448560f7a7b70fbba873c1
Mateusz Milczarek <mateusz.milczarek1@uth.pl>
2025-04-05 12:39:52 +0200

    Dodanie wyjątku InputMismatchException gdy użytkownik poda inną wartość niż liczbę

--- src/main/java/Kalkulator.java
+++ src/main/java/Kalkulator.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Kalkulator  {
@@ -5,46 +6,49 @@ public class Kalkulator  {
         Scanner scanner = new Scanner(System.in);
         boolean isActive = false;
         while(!isActive){
-            System.out.print("Podaj pierwszą liczbę: ");
-            int a = scanner.nextInt();
-            System.out.print("Podaj drugą liczbę: ");
-            int b = scanner.nextInt();
-            scanner.nextLine();
-            System.out.println("Jaka operację chcesz wykonać? (+, -, *, /): ");
-            String operacja = scanner.nextLine();
-            String dzialanie = String.format("%d %s %d = ", a, operacja, b);
-            int wynik;
-            double wynikDzielenia;
-            switch(operacja) {
-                case "+":
-                    wynik = a + b;
-                    System.out.println(dzialanie + wynik);
-                    break;
-                case "-":
-                    wynik = a - b;
-                    System.out.println(dzialanie + wynik);
-                    break;
-                case "*":
-                    wynik = a * b;
-                    System.out.println(dzialanie + wynik);
-                    break;
-                case "/":
-                    if (b != 0) {
-                        wynikDzielenia = (double) a / b;
-                        System.out.println(dzialanie + wynikDzielenia);
-                    } else {
-                        System.out.println("Nie wolno dzielić przez 0!");
-                    }
-                    break;
-                default:
-                    System.out.println("Nieznany znak");
+            try{
+                System.out.print("Podaj pierwszą liczbę: ");
+                int a = scanner.nextInt();
+                System.out.print("Podaj drugą liczbę: ");
+                int b = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println("Jaka operację chcesz wykonać? (+, -, *, /): ");
+                String operacja = scanner.nextLine();
+                String dzialanie = String.format("%d %s %d = ", a, operacja, b);
+                int wynik;
+                double wynikDzielenia;
+                switch(operacja) {
+                    case "+":
+                        wynik = a + b;
+                        System.out.println(dzialanie + wynik);
+                        break;
+                    case "-":
+                        wynik = a - b;
+                        System.out.println(dzialanie + wynik);
+                        break;
+                    case "*":
+                        wynik = a * b;
+                        System.out.println(dzialanie + wynik);
+                        break;
+                    case "/":
+                        if (b != 0) {
+                            wynikDzielenia = (double) a / b;
+                            System.out.println(dzialanie + wynikDzielenia);
+                        } else {
+                            System.out.println("Nie wolno dzielić przez 0!");
+                        }
+                        break;
+                    default:
+                        System.out.println("Nieznany znak");
+                }
+            }catch(InputMismatchException e){
+                System.out.println("Podałeś niepoprawną liczbę!");
             }
-            System.out.println("Wciśnij 'T' jeżeli chcesz wyjść z programu");
-            System.out.println("Wciśnij dowolny klawisz jeżeli chcesz zostać ");
+            System.out.println("Wciśnij 'T' jeżeli chcesz wyjść z programu...");
+            System.out.println("Wciśnij dowolny klawisz jeżeli chcesz wykonać kolejne działanie...");
             String wyjscie = scanner.nextLine();
             if(wyjscie.equals("T") || wyjscie.equals("t"))
                 isActive = true;
         }
-
     }
 }
